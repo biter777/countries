41d28de6784c17a41f46f13b0e0b3c8ebb130474
biter777 <33280398+biter777@users.noreply.github.com>
2019-04-25 19:22:57 +0300

    Add support region, capital and call codes info

--- countries.go
+++ countries.go
@@ -7,6 +7,39 @@ import (
 	"fmt"
 )
 
+/*********************** E X A M P L E ***********************\
+func main() {
+	countryJapan := countries.Japan
+	fmt.Printf("Country name in english: %v\n", countryJapan)
+	fmt.Printf("Country name in russian: %v\n", countryJapan.StringRus())
+	fmt.Printf("Country digit code: %d\n", countryJapan)
+	fmt.Printf("Country Alpha-2 code: %v\n", countryJapan.Alpha2())
+	fmt.Printf("Country Alpha-3 code: %v\n", countryJapan.Alpha3())
+	fmt.Printf("Country Capital: %v\n", countryJapan.Capital())
+	fmt.Printf("Country region name: %v\n", countryJapan.Region())
+	fmt.Printf("Country region code: %d\n", countryJapan.Region())
+	fmt.Printf("Country emoji: %v\n\n", countryJapan.Emoji())
+
+	currencyJapan := countryJapan.Currency()
+	fmt.Printf("Country Currency name in english: %v\n", currencyJapan)
+	fmt.Printf("Country Currency digit code: %d\n", currencyJapan)
+	fmt.Printf("Country Currency Alpha code: %v\n", currencyJapan.Alpha())
+	fmt.Printf("Country Currency emoji: %v\n", currencyJapan.Emoji())
+	fmt.Printf("Country of Currency %v: %v\n\n", currencyJapan, currencyJapan.Countries())
+
+	// OR you can alternative use:
+	japanInfo := countries.Japan.Info()
+	fmt.Printf("Country name in english: %v\n", japanInfo.Name)
+	fmt.Printf("Country digit code: %d\n", japanInfo.Code)
+	fmt.Printf("Country Alpha-2 code: %v\n", japanInfo.Alpha2)
+	fmt.Printf("Country Alpha-3 code: %v\n", japanInfo.Alpha3)
+	fmt.Printf("Country Capital: %v\n", japanInfo.Capital)
+	fmt.Printf("Country region name: %v\n", japanInfo.Region)
+	fmt.Printf("Country region code: %d\n", japanInfo.Region)
+	fmt.Printf("Country emoji: %v\n", japanInfo.Emoji)
+}
+\************************* E N J O Y ***********************/
+
 // CountryCode - country code (250 countries). Three codes present, for example Russia == RU == RUS == 643.
 type CountryCode int64 // int64 for database/sql/driver.Valuer compatibility
 
