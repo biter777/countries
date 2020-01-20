440ac84b9989f36b61f9067ae2e32442af5e09b4
biter777 <biter777@gmail.com>
2020-01-20 16:42:38 +0300

    Add comments and tests

--- capitalsconst.go
+++ capitalsconst.go
@@ -1,4 +1,3 @@
-// Package countries - ISO 3166 (ISO3166-1, ISO3166, Digit, Alpha-2 and Alpha-3) countries codes and names (on eng and rus), ISO 4217 currency designators, ITU-T E.164 IDD calling phone codes, countries capitals, UN M.49 regions codes, ccTLD countries domains, IOC/NOC and FIFA letters codes, VERY FAST, NO maps[], NO slices[], NO external links/files/data, NO interface{}, NO specific dependencies, Databases compatible, Emoji countries flags and currencies support, full support ISO-3166-1, ISO-4217, ITU-T E.164, Unicode CLDR and ccTLD standarts. Full support ISO-3166-1, ISO-4217, ITU-T E.164, Unicode CLDR and ccTLD standarts.
 package countries
 
 // TypeCapitalCode for Typer interface
