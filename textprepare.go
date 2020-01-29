b0b16f5e737fe2b1848ac62ee0032e5ba3bb6a9a
biter777 <biter777@gmail.com>
2020-01-29 15:23:00 +0300

    textPrepare() func improved

--- textprepare.go
+++ textprepare.go
@@ -1,27 +1,30 @@
 package countries
 
 import (
-	"regexp"
+	"io"
 	"strings"
+	"unicode"
 )
 
-// rePrepare - for func textPrepare()
-var rePrepare *regexp.Regexp
-
-// stPrepare - for func textPrepare()
-var stPrepare *strings.Replacer
-
-func init() {
-	rePrepare = regexp.MustCompile("\n|\t|\r|\f|\v|\"|`|'|’|′|´|῾|᾿|″|˝|̏ |̏‴|“|”|‘| ҃|‹|›|«|»|^|ˆ|!|¡|¿|؟|#|№|÷|[|]|{|}|⟨|⟩|\\|/|⁄|¦|;|:|,|·|•|°|º|ﾟ|˚|˳|%|‰|‱|¨|…| | | |-|‐|‑|‒|―|—|–|~|_|¯|@|¶|§|©|®|℠|™|℗")
-	stPrepare = strings.NewReplacer("&", "AND", ")", "", "?", "", ".", "", "|", "", "+", "", "*", "")
-}
-
+// NOTE: it works very more faster than strings.Replacer and regexp.Regexp
 func textPrepare(text string) string {
-	text = rePrepare.ReplaceAllString(text, "")
 	indx := strings.Index(text, "(")
 	if indx > -1 {
 		text = text[:indx]
 	}
-	text = stPrepare.Replace(text)
+
+	reader := strings.NewReader(text)
+	text = ""
+
+	for {
+		r, _, err := reader.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if unicode.IsLetter(r) {
+			text += string(r)
+		}
+	}
+
 	return strings.ToUpper(text)
 }
